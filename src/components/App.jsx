9bb706cf94a50bcf2f7648797621e59c86456a1c
Dimitrina Beleva <40005046+dimitrina-beleva@users.noreply.github.com>
2018-07-19 21:36:19 +0300

    Added film (#2)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,7 +15,8 @@ const ITEMS = [
   { id: 2, title: 'Dunkirk', plot: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.' },
   { id: 3, title: 'Logan', plot: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.' },
   { id: 4, title: 'Get Out', plot: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.' },
-  { id: 5, title: 'Wonder Woman', plot: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.' }
+  { id: 5, title: 'Wonder Woman', plot: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.' },
+  { id: 6, title: 'XMAN', plot: 'XX.' }
 ];
 
 // http://www.omdbapi.com/
