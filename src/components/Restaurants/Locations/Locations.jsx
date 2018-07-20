ac3aa7c8cbe3d5dce322af59ca062a9803b20571
Plamen Draganov <pdragatc@training.local>
2018-07-20 21:03:35 +0300

    merge

--- src/components/Restaurants/Locations/Locations.jsx
+++ src/components/Restaurants/Locations/Locations.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import Description from './Description';
 import Item from './Item';
-import ListItems from './ListItems'
+import ListItems from './ListItems';
 
 const Locations = () => (
-    <div>
-      <p>Locations</p>
-    </div>
-  );
+  <div>
+    <p>Locations</p>
+  </div>
+);
 
 export default { Locations, Description, Item, ListItems };
