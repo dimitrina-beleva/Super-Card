c48da3baa0ffee4c53fb51f7d4d1ebe851e1f07d
Philip Kovachev <36931694+philipkovachev9@users.noreply.github.com>
2018-07-20 21:11:41 +0300

    Feature/newrouter (#19)
    * css can now be loaded
    
    * css can now be imported
    
    * Router now works
    
    * pages now link together

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
 import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
 import React from 'react';
+import * as pages from './pages';
 
 const Home = () => <h1>Home</h1>;
-const About = () => <h1>About</h1>;
 
 const App = () => (
   <Router>
     <div>
-      <Link to="/">Home</Link> <Link to={{ pathname: '/about' }}>About</Link> <Link to="/contact">Contact</Link>
+      <Link to="/">Home</Link> <Link to={{ pathname: '/auto' }}>Auto</Link> <Link to="/beauty">Beauty</Link>{' '}
+      <Link to="/clothes">Clothes</Link> <Link to="/tech">Tech</Link>
+      <Link to="/sport">Sport</Link> <Link to="/restaurants">Restaurants</Link>
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/contact" render={() => <h1>Contact Us</h1>} />
+        <Route path="/auto" component={pages.Auto} />
+        <Route path="/beauty" render={pages.Beauty} />
+        <Route path="/clothes" render={pages.Clothes} />
+        <Route path="/tech" render={pages.Tech} />
+        <Route path="/sport" render={pages.Sport} />
+        <Route path="/restaurants" render={pages.Restaurants} />
         <Route render={() => <h1>Page not found</h1>} />
       </Switch>
     </div>
