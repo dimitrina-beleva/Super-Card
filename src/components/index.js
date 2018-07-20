5ee143c73703d2c1779287f4583bbfbc21b8b4d1
Denitsa Dobreva <denitsa.dr.dobreva@gmail.com>
2018-07-20 21:02:54 +0300

    lowercase dir (#17)

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 import Header from './Header';
 import Footer from './Footer';
-import Locations from './Restaurants/Locations/Locations';
-import ChatBot from './Restaurants/ChatBot/ChatBot'
+import Locations from './restaurants/locations/Locations';
+import ChatBot from './restaurants/chatbot/ChatBot'
 
 export { Header, Footer, Locations, ChatBot };
