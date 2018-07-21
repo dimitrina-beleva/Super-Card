51af5d3f4c86cd39300c6da56126485f76f4f388
Dimitrina Beleva <40005046+dimitrina-beleva@users.noreply.github.com>
2018-07-21 18:08:07 +0300

    Added project details (#22)

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 import Header from './Header';
 import Footer from './Footer';
 import Locations from './restaurants/locations/Locations';
-import ChatBot from './restaurants/chatbot/ChatBot'
+import ChatBot from './restaurants/chatbot/ChatBot';
 
 export { Header, Footer, Locations, ChatBot };
