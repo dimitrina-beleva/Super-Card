cafbec3af53931cb34e7aa0fc6b3ef8b0b07bf39
Denitsa Dobreva <denitsa.dr.dobreva@gmail.com>
2018-07-23 01:38:32 +0300

    code refactoring (#28)

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,57 +1,61 @@
 import React from 'react';
 
 const Footer = () => (
-  <div className="wrapper">
+  <div className="footer">
     <div className="footer-container">
-      <div className="about">
+      <div className="footer-about">
         <h2>About</h2>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-          dolore dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          consequat.
+          SuperCard is a card, that entitles the holder to discounts on the prices of some products or services. Cards
+          may be issued as part of a loyalty program and may be offered by clubs or associations who negotiate on behalf
+          of all members to obtain benefits.
         </p>
       </div>
-      <div className="navigation">
-        <h2> Navigatoin</h2>
-        <p>
-          <a href="/">Home</a>
-        </p>
-        <p>
-          <a href="/beauty">Beauty</a>
-        </p>
-        <p>
-          <a href="/restaurants">Restaurants</a>
-        </p>
-        <p>
-          <a href="/clothes">Clothes</a>
-        </p>
-        <p>
-          <a href="/sport">Sport</a>
-        </p>
-        <p>
-          <a href="/tech">Tech</a>
-        </p>
-        <p>
-          <a href="/auto">Auto</a>
-        </p>
+      <div className="footer-navigation">
+        <h2> Navigation</h2>
+        <ul className="footer-links">
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/beauty">Beauty</a>
+          </li>
+          <li>
+            <a href="/restaurants">Restaurants</a>
+          </li>
+          <li>
+            <a href="/clothes">Clothes</a>
+          </li>
+          <li>
+            <a href="/sport">Sport</a>
+          </li>
+          <li>
+            <a href="/tech">Tech</a>
+          </li>
+          <li>
+            <a href="/auto">Auto</a>
+          </li>
+        </ul>
       </div>
-      <div className="social">
+      <div className="footer-social">
         <h2>Social</h2>
-        <p>
-          <a href="https://www.facebook.com/">Facebook</a>
-        </p>
-        <p>
-          <a href="https://www.instagram.com/">Instagram</a>
-        </p>
-        <p>
-          <a href="https://www.twitter.com/">Twitter</a>
-        </p>
-        <p>
-          <a href="https://plus.google.com">Google+</a>
-        </p>
+        <ul className="footer-links">
+          <li>
+            <a href="https://www.facebook.com/">Facebook</a>
+          </li>
+          <li>
+            <a href="https://www.instagram.com/">Instagram</a>
+          </li>
+          <li>
+            <a href="https://www.twitter.com/">Twitter</a>
+          </li>
+          <li>
+            <a href="https://plus.google.com">Google+</a>
+          </li>
+        </ul>
       </div>
     </div>
-    <p className="center">&copy; 2018 All rights reserved. Designed by Smart Investments Team :)</p>
+    <div className="copyright">&copy; 2018 All rights reserved. Designed by Smart Investments Team :)</div>
   </div>
 );
 
